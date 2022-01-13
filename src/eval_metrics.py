686490789ea31a2721332b97db7814781009e6f8
Lucas Goncalves <70816275+ilucasgoncalves@users.noreply.github.com>
2022-01-13 18:41:10 -0300

    Update eval_metrics.py

--- src/eval_metrics.py
+++ src/eval_metrics.py
@@ -1,5 +1,7 @@
 import numpy as np
 from sklearn.metrics import f1_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
 
 
 def scores(results, truths):
@@ -18,7 +20,19 @@ def scores(results, truths):
     test_truth_i = np.array(test_truth)
     f1ma = f1_score(test_truth_i, test_preds_i, average='macro')
     f1mi = f1_score(test_truth_i, test_preds_i, average='micro')
-    print('F1 Macro = {:5.3f}'.format(f1ma))
-    print('F1 Micro = {:5.3f}'.format(f1mi))
+    pre_ma = precision_score(test_truth_i, test_preds_i, average='macro')
+    pre_mi = precision_score(test_truth_i, test_preds_i, average='micro')
+    re_ma = recall_score(test_truth_i, test_preds_i, average='macro')
+    re_mi = recall_score(test_truth_i, test_preds_i, average='micro')
+    
+    print('F1-Score Macro = {:5.3f}'.format(f1ma))
+    print('F1-Score Micro = {:5.3f}'.format(f1mi))
+    print('-------------------------')
+    print('Precision Macro = {:5.3f}'.format(pre_ma))
+    print('Precision Micro = {:5.3f}'.format(pre_mi))
+    print('-------------------------')
+    print('Recall Macro = {:5.3f}'.format(re_ma))
+    print('Recall Micro = {:5.3f}'.format(re_mi))
+    print('-------------------------')
 
 
